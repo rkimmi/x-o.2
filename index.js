8f97e336306405d17ff19f7879b234a1d56decbb
rkimmi <kimmirindel97@gmail.com>
2018-05-07 18:42:03 +1200

    added column and diagonal win conditions

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function makeMove (element) {
 
 function checkWin () {
     checkRows ()
+    checkColumns ()
+    checkDiagonal ()
 }
 
 function checkRows () {
@@ -28,7 +30,6 @@ function checkRows () {
         let winner = $('#blockOne').text()
         $('p').text(`Player ${winner} wins!`)
     }
-
     if ($('#blockFour').text().length > 0 &&
         $('#blockFour').text() === $('#blockFive').text() &&
         $('#blockFive').text() === $('#blockSix').text()) {
@@ -41,9 +42,43 @@ function checkRows () {
         let winner = $('#blockSeven').text()
         $('p').text(`Player ${winner} wins!`)
     }
-
 }
 
+function checkColumns () {
+    if ($('#blockOne').text().length > 0 &&
+        $('#blockOne').text() === $('#blockFour').text() &&
+        $('#blockFour').text() === $('#blockSeven').text()) {
+        let winner = $('#blockOne').text()
+        $('p').text(`Player ${winner} wins!`)
+    }
+    if ($('#blockTwo').text().length > 0 &&
+        $('#blockTwo').text() === $('#blockFive').text() &&
+        $('#blockFive').text() === $('#blockEight').text()) {
+        let winner = $('#blockFour').text()
+        $('p').text(`Player ${winner} wins!`)
+    }
+    if ($('#blockThree').text().length > 0 &&
+        $('#blockThree').text() === $('#blockSix').text() &&
+        $('#blockSix').text() === $('#blockNine').text()) {
+        let winner = $('#blockThree').text()
+        $('p').text(`Player ${winner} wins!`)
+    }
+}
+
+function checkDiagonal () {
+    if ($('#blockOne').text().length > 0 &&
+        $('#blockOne').text() === $('#blockFive').text() &&
+        $('#blockFive').text() === $('#blockNine').text()) {
+        let winner = $('#blockOne').text()
+        $('p').text(`Player ${winner} wins!`)
+    }
+    if ($('#blockThree').text().length > 0 &&
+        $('#blockThree').text() === $('#blockFive').text() &&
+        $('#blockFive').text() === $('#blockSeven').text()) {
+        let winner = $('#blockThree').text()
+        $('p').text(`Player ${winner} wins!`)
+    }
+}
 
 
 

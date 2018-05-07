967c411bff7d9c74608e1d275b58b98f5d72f47b
rkimmi <kimmirindel97@gmail.com>
2018-05-07 14:41:53 +1200

    'i love *this*'

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // $(selector).action()
 
-// $("div").hide()
+const playerOne = 'X' //
+const playerTwo = '' //
+
+function makeMove (element) {
+    let playerOneTurn = true
+    let playerTwoTurn = false
+
+    $(element).text(playerOne);
+}
